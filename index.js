949957ceb4b59c2f250836e7033d143b27858b6a
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-12-18 11:18:43 +0530

    61

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Serve static assets
 app.use("/assets", express.static(path.join(__dirname, "src", "assets")));
